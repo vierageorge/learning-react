da283bca57175ed48c20b8c1d43512bec28a77c0
Jorge Viera <joviera@gmail.com>
2020-08-21 06:59:53 -0500

    Introduces component's state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import './App.css'
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age:26}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi! This is the new React App</h1>
         <p>This is really working!</p>
-        <Person name="Jorge" age="33" />
-        <Person name="Irina" age="30">My Hobbies: Yoga</Person>
-        <Person name="Arturo" age="28" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     )
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
