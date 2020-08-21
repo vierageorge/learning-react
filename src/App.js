84bbb7103739c829e2c78fa59c036f39f94bea27
Jorge Viera <joviera@gmail.com>
2020-08-21 06:48:52 -0500

    :tada: Children prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
 import Person from './Person/Person'
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <h1>Hi! This is the new React App</h1>
         <p>This is really working!</p>
         <Person name="Jorge" age="33" />
         <Person name="Irina" age="30">My Hobbies: Yoga</Person>
-        <Person name="Arturo" age="28"/>
+        <Person name="Arturo" age="28" />
       </div>
     )
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
